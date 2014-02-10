1e98da375f207eab65c9e710ef4a1975c6c874bb
arturalkaim <arturalkaim@gmail.com>
2014-02-10 19:38:36 +0000

    Print Items

---
+++ EasyStockWebProject/src/epic/easystock/client/components/PrintItems.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package epic.easystock.client.components;
+
+/**
+ * @author artur
+ *
+ */
+public class PrintItems extends AbstractContent {
+
+	/**
+	 * 
+	 */
+	public PrintItems() {
+		loadModule();
+	}
+
+	@Override
+	public void loadModule() {
+		super.loadModule();
+		
+		
+		
+		
+	}
+
+	@Override
+	public void reloadModule() {
+		// TODO Auto-generated method stub
+		super.reloadModule();
+	}
+	
+	
+
+}
