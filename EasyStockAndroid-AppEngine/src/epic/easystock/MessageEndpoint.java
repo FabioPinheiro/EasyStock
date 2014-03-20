9b03a99affd79e432216dd8de564f8161ea167cd
aovelhanegra <rafael.reia@gmail.com>
2014-03-20 11:42:26 +0000

    API key added

--- EasyStockAndroid-AppEngine/src/epic/easystock/MessageEndpoint.java
+++ EasyStockAndroid-AppEngine/src/epic/easystock/MessageEndpoint.java
@@ -53,7 +53,7 @@ public class MessageEndpoint {
    * Google Cloud Messaging from your AppEngine application even if you are
    * using a App Engine's local development server.
    */
-  private static final String API_KEY = "";
+  private static final String API_KEY = "AIzaSyAqJOGIfANnOzHcmdhUO4mnqYEyARNqBNc";
 
   private static final DeviceInfoEndpoint endpoint = new DeviceInfoEndpoint();
   
