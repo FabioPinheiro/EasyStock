53ef4233239c62e267b54a02dff8a5a61b81de37
arturalkaim <arturalkaim@gmail.com>
2014-03-24 17:29:45 +0000

    Bug Corrected

--- EasyStockAndroid/src/epic/easystock/ProductListActivity.java
+++ EasyStockAndroid/src/epic/easystock/ProductListActivity.java
@@ -7,8 +7,14 @@ import java.io.IOException;
 import java.util.Date;
 
 import android.os.AsyncTask;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.content.Context;
+import epic.easystock.RegisterActivity.State;
 import epic.easystock.itemendpoint.*;
 import epic.easystock.itemendpoint.model.*;
 
@@ -18,13 +24,29 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.jackson.JacksonFactory;
 
 public class ProductListActivity extends Activity {
+	
+	
+	private OnTouchListener addListener = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_product_list);
 		
-		new EndpointsTask().execute(getApplicationContext());
+		
+		Button addItemButton = (Button) findViewById(R.id.button1);
+
+	    addListener = new OnTouchListener() {
+	      @Override
+	      public boolean onTouch(View v, MotionEvent event) {
+	    	  new EndpointsTask().execute(getApplicationContext());
+			return true;
+	      }
+	    };
+		
+	    addItemButton.setOnTouchListener(addListener);
+
+		
 	}
 
 	public class EndpointsTask extends AsyncTask<Context, Integer, Long> {
@@ -39,12 +61,15 @@ public class ProductListActivity extends Activity {
 			Itemendpoint endpoint = CloudEndpointUtils.updateBuilder(
 					endpointBuilder).build();
 			try {
-				 
+
 				ItemData item = new ItemData();
-				String name = ((EditText)findViewById(R.id.itemName)).getText().toString(); 
+				String name = ((EditText) findViewById(R.id.itemName))
+						.getText().toString();
 				item.setName(name);
-				
-				String description = ((EditText)findViewById(R.id.descriptionText)).getText().toString();;
+
+				String description = ((EditText) findViewById(R.id.descriptionText))
+						.getText().toString();
+				;
 				item.setDescription(description);
 				ItemData result = endpoint.insertItem(item).execute();
 			} catch (IOException e) {
