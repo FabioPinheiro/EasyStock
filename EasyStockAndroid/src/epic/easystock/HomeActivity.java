87085d5b6bdb43299c3bb0cf1ef69a356e13cd9e
FabioPinheiro <fabiomgpinheiro@gmail.com>
2014-03-24 15:19:31 +0000

    ProductListActivity

--- EasyStockAndroid/src/epic/easystock/HomeActivity.java
+++ EasyStockAndroid/src/epic/easystock/HomeActivity.java
@@ -1,13 +1,10 @@
 package epic.easystock;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class HomeActivity extends Activity {
 
@@ -23,6 +20,11 @@ public class HomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		//getMenuInflater().inflate(R.menu.home, menu);
 		return true;
+	}
+	
+	public void changeToProductListActivity(View view) {
+		Intent intent = new Intent(this, ProductListActivity.class);
+		startActivity(intent);
 	}
 
 }
---
+++ EasyStockAndroid/src/epic/easystock/ProductListActivity.java
@@ -0,0 +1,14 @@
+package epic.easystock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProductListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_product_list);
+	}
+
+}
