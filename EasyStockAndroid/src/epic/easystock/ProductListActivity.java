cfa4bab1e11c21fe06840b675ebd768300ece482
arturalkaim <arturalkaim@gmail.com>
2014-03-24 21:27:55 +0000

    Simple add Item

--- EasyStockAndroid/src/epic/easystock/ProductListActivity.java
+++ EasyStockAndroid/src/epic/easystock/ProductListActivity.java
@@ -25,57 +25,44 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.jackson.JacksonFactory;
 
 public class ProductListActivity extends Activity {
-	
-	
+
 	private OnTouchListener addListener = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_product_list);
-		
+		new EndpointsTask().execute();
 		/*
-		Button addItemButton = (Button) findViewById(R.id.button1);
+		 * Button addItemButton = (Button) findViewById(R.id.button1);
+		 * 
+		 * addListener = new OnTouchListener() {
+		 * 
+		 * @Override public boolean onTouch(View v, MotionEvent event) {
+		 * Itemendpoint.Builder endpointBuilder = new Itemendpoint.Builder(
+		 * AndroidHttp.newCompatibleTransport(), new JacksonFactory(), new
+		 * HttpRequestInitializer() { public void initialize(HttpRequest
+		 * httpRequest) { } }); Itemendpoint endpoint =
+		 * CloudEndpointUtils.updateBuilder( endpointBuilder).build(); try {
+		 * 
+		 * ItemData item = new ItemData(); String name = ((EditText)
+		 * findViewById(R.id.itemName)) .getText().toString();
+		 * item.setName(name);
+		 * 
+		 * String description = ((EditText) findViewById(R.id.descriptionText))
+		 * .getText().toString(); ; item.setDescription(description); ItemData
+		 * result = endpoint.insertItem(item).execute(); } catch (IOException e)
+		 * { e.printStackTrace(); } return true; } };
+		 * 
+		 * addItemButton.setOnTouchListener(addListener);
+		 */
 
-		addListener = new OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				Itemendpoint.Builder endpointBuilder = new Itemendpoint.Builder(
-						AndroidHttp.newCompatibleTransport(), new JacksonFactory(),
-						new HttpRequestInitializer() {
-							public void initialize(HttpRequest httpRequest) {
-							}
-						});
-				Itemendpoint endpoint = CloudEndpointUtils.updateBuilder(
-						endpointBuilder).build();
-				try {
-
-					ItemData item = new ItemData();
-					String name = ((EditText) findViewById(R.id.itemName))
-							.getText().toString();
-					item.setName(name);
-
-					String description = ((EditText) findViewById(R.id.descriptionText))
-							.getText().toString();
-					;
-					item.setDescription(description);
-					ItemData result = endpoint.insertItem(item).execute();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				return true;
-			}
-		};
-		
-		addItemButton.setOnTouchListener(addListener);*/
-
-		
 	}
-	
+
 	public void addXPTO(View view) {
-		new EndpointsTask().execute(getApplicationContext());
+		
 	}
-	
+
 	public class EndpointsTask extends AsyncTask<Context, Integer, Long> {
 		protected Long doInBackground(Context... contexts) {
 
@@ -90,14 +77,14 @@ public class ProductListActivity extends Activity {
 			try {
 
 				ItemData item = new ItemData();
-				String name = ((EditText) findViewById(R.id.itemName))
-						.getText().toString();
+				String name = "Batatas";/*((EditText) findViewById(R.id.itemName))
+						.getText().toString();*/
 				item.setName(name);
 
-				String description = ((EditText) findViewById(R.id.descriptionText))
-						.getText().toString();
-				;
+				String description = "uns legumes bueda bons que veem da terra!";/*((EditText) findViewById(R.id.descriptionText))
+						.getText().toString();*/
 				item.setDescription(description);
+				
 				ItemData result = endpoint.insertItem(item).execute();
 			} catch (IOException e) {
 				e.printStackTrace();
