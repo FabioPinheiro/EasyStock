aade50264507c9fadae69ac05d3b3aad2a9ee28e
arturalkaim <arturalkaim@gmail.com>
2014-03-25 23:14:16 +0000

    Montes de Entities Criadas

---
+++ EasyStockAndroid-AppEngine/src/epic/easystock/User.java
@@ -0,0 +1,42 @@
+package epic.easystock;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.google.appengine.api.datastore.Key;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Key key;
+	
+	public Key getKey() {
+		return key;
+	}
+
+	private String nick;
+	
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	
+	
+}
---
+++ EasyStockAndroid-AppEngine/src/epic/easystock/UserEndpoint.java
@@ -0,0 +1,168 @@
+package epic.easystock;
+
+import epic.easystock.EMF;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.response.CollectionResponse;
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.datanucleus.query.JPACursorHelper;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+import javax.inject.Named;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+@Api(name = "userendpoint", namespace = @ApiNamespace(ownerDomain = "easystock.epic", ownerName = "easystock.epic", packagePath = ""))
+public class UserEndpoint {
+
+	/**
+	 * This method lists all the entities inserted in datastore.
+	 * It uses HTTP GET method and paging support.
+	 *
+	 * @return A CollectionResponse class containing the list of all entities
+	 * persisted and a cursor to the next page.
+	 */
+	@SuppressWarnings({ "unchecked", "unused" })
+	@ApiMethod(name = "listUser")
+	public CollectionResponse<User> listUser(
+			@Nullable @Named("cursor") String cursorString,
+			@Nullable @Named("limit") Integer limit) {
+
+		EntityManager mgr = null;
+		Cursor cursor = null;
+		List<User> execute = null;
+
+		try {
+			mgr = getEntityManager();
+			Query query = mgr.createQuery("select from User as User");
+			if (cursorString != null && cursorString != "") {
+				cursor = Cursor.fromWebSafeString(cursorString);
+				query.setHint(JPACursorHelper.CURSOR_HINT, cursor);
+			}
+
+			if (limit != null) {
+				query.setFirstResult(0);
+				query.setMaxResults(limit);
+			}
+
+			execute = (List<User>) query.getResultList();
+			cursor = JPACursorHelper.getCursor(execute);
+			if (cursor != null)
+				cursorString = cursor.toWebSafeString();
+
+			// Tight loop for fetching all entities from datastore and accomodate
+			// for lazy fetch.
+			for (User obj : execute)
+				;
+		} finally {
+			mgr.close();
+		}
+
+		return CollectionResponse.<User> builder().setItems(execute)
+				.setNextPageToken(cursorString).build();
+	}
+
+	/**
+	 * This method gets the entity having primary key id. It uses HTTP GET method.
+	 *
+	 * @param id the primary key of the java bean.
+	 * @return The entity with primary key id.
+	 */
+	@ApiMethod(name = "getUser")
+	public User getUser(@Named("id") Long id) {
+		EntityManager mgr = getEntityManager();
+		User user = null;
+		try {
+			user = mgr.find(User.class, id);
+		} finally {
+			mgr.close();
+		}
+		return user;
+	}
+
+	/**
+	 * This inserts a new entity into App Engine datastore. If the entity already
+	 * exists in the datastore, an exception is thrown.
+	 * It uses HTTP POST method.
+	 *
+	 * @param user the entity to be inserted.
+	 * @return The inserted entity.
+	 */
+	@ApiMethod(name = "insertUser")
+	public User insertUser(User user) {
+		EntityManager mgr = getEntityManager();
+		try {
+			if (containsUser(user)) {
+				throw new EntityExistsException("Object already exists");
+			}
+			mgr.persist(user);
+		} finally {
+			mgr.close();
+		}
+		return user;
+	}
+
+	/**
+	 * This method is used for updating an existing entity. If the entity does not
+	 * exist in the datastore, an exception is thrown.
+	 * It uses HTTP PUT method.
+	 *
+	 * @param user the entity to be updated.
+	 * @return The updated entity.
+	 */
+	@ApiMethod(name = "updateUser")
+	public User updateUser(User user) {
+		EntityManager mgr = getEntityManager();
+		try {
+			if (!containsUser(user)) {
+				throw new EntityNotFoundException("Object does not exist");
+			}
+			mgr.persist(user);
+		} finally {
+			mgr.close();
+		}
+		return user;
+	}
+
+	/**
+	 * This method removes the entity with primary key id.
+	 * It uses HTTP DELETE method.
+	 *
+	 * @param id the primary key of the entity to be deleted.
+	 */
+	@ApiMethod(name = "removeUser")
+	public void removeUser(@Named("id") Long id) {
+		EntityManager mgr = getEntityManager();
+		try {
+			User user = mgr.find(User.class, id);
+			mgr.remove(user);
+		} finally {
+			mgr.close();
+		}
+	}
+
+	private boolean containsUser(User user) {
+		EntityManager mgr = getEntityManager();
+		boolean contains = true;
+		try {
+			User item = mgr.find(User.class, user.getKey());
+			if (item == null) {
+				contains = false;
+			}
+		} finally {
+			mgr.close();
+		}
+		return contains;
+	}
+
+	private static EntityManager getEntityManager() {
+		return EMF.get().createEntityManager();
+	}
+
+}
