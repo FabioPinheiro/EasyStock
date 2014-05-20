f5dd2dc1e45846d0d3af484ed3d593bf37a12d61
arturalkaim <arturalkaim@gmail.com>
2014-05-21 00:24:33 +0100

    AddPantry Commit

--- EasyStockAndroid/src/epic/easystock/activitys/PantyActivity.java
+++ EasyStockAndroid/src/epic/easystock/activitys/PantyActivity.java
@@ -14,13 +14,16 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import epic.easystock.R;
+import epic.easystock.apiEndpoint.model.Product;
 import epic.easystock.assist.MetaProductAdapter;
 import epic.easystock.data.LocalMetaProduct;
 import epic.easystock.data.PantriesDBAdapter;
 import epic.easystock.data.PantriesDBAdapter.PantryDB;
 import epic.easystock.io.EndPointCall;
+import android.content.Intent;
 
 public class PantyActivity extends ListActivity {
+	protected static final int ADDPRODUCT = 666;
 	private final String LOG_TAG = this.getClass().getCanonicalName();
 	private PantriesDBAdapter.PantryDB pantryDB;
 	String mail;
@@ -61,15 +64,73 @@ public class PantyActivity extends ListActivity {
 		alert.show();
 		
 		addProduct.setOnClickListener(new OnClickListener() {
+			Intent intent = new Intent(EndPointCall.getGlobalContext(),AddProductToPantryActivity.class);
 			@Override
 			public void onClick(View v) {
+				intent.putExtra("PANTRYNAME", selectedPantryName);
+				startActivityForResult(intent, ADDPRODUCT);
+	/*			
+				Log.i(LOG_TAG, "new AddProductTask: " + mail);
+				Long productBarCode = Long.valueOf(((EditText) findViewById(R.id.NumberId)).getText().toString());
+				EndPointCall.addProductToPantryTask(adapter, pantryDB, productBarCode);//FIXME BARCODE*/
+			}
+		});
+		
+		
+		final AlertDialog.Builder addproduct = new AlertDialog.Builder(this);
+		addproduct.setTitle("Change Product");
+		addproduct.setItems(pantreisName, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				selectedPantryName = pantreisName[which];//FIXME
+				pantryDB = EndPointCall.getPantryDB(selectedPantryName);
+				EndPointCall.listPantryProductTask(adapter, pantryDB); //FIXME
+				
+				//NÂO gosto
+			}
+		});
+		
+		addproduct.setNeutralButton("new",new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				
+			}
+		});
+		addproduct.setOnCancelListener(new OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				aux.finish();
+			}
+		});
+	/*	
+		addProduct.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				addproduct.show();
+				
 				Log.i(LOG_TAG, "new AddProductTask: " + mail);
 				Long productBarCode = Long.valueOf(((EditText) findViewById(R.id.NumberId)).getText().toString());
 				EndPointCall.addProductToPantryTask(adapter, pantryDB, productBarCode);//FIXME BARCODE
 			}
-		});
+		});*/
 	}
-	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+	    if (requestCode == ADDPRODUCT) {
+	        if(resultCode == RESULT_OK){
+	            String name=data.getStringExtra("PRODNAME");
+	            String description=data.getStringExtra("PRODDESCRIPTION");
+	            Long barCode=data.getLongExtra("PRODBARCODE", -1l);
+				Product result = new Product();
+				result.setBarCode(barCode);
+				result.setDescription(description);
+				result.setName(name);
+	        }
+	        if (resultCode == RESULT_CANCELED) {
+	            //Write your code if there's no result
+	        }
+	    }
+	}//onActivityResult
 	@Override
 	protected void onStop() {
 		super.onStop();
